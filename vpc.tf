30ad307c35d26df668e2abe8b4e888e1b3a5da58
Rajesh Radhakrishnan <rajeshr_2020@outlook.com>
2023-08-28 00:36:06 +0530

    vpc-with-subnets

---
+++ vpc.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Learning-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+#Internet Gatewat Creation
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+
+ tags = {
+   Name = "Learning-VPC"
+ }
+}
+
+#Creation of 2nd route table as a best practice so that 1 route table can manage internal traffic and other can provide with Internet Gateway access
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+# Associating Public Subnets to Second Route table
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
