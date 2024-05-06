7decd60b1e985cf2cae3c33e23741447c5d2c0dd
rajesh180793 <62793093+rajesh180793@users.noreply.github.com>
2024-05-06 22:11:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,29 @@
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = null
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  default     = null
 }
 
 variable "azs" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = null
+}
+
+variable "cidr_blocks" {
+  type        = string
+  description = "Availability Zones"
+  default     = ""
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Availability Zones"
+  default     = ""
 }
