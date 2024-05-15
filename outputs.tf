8232a8a29de68d4e55a7fc5070985c9becb985b7
rajesh180793 <62793093+rajesh180793@users.noreply.github.com>
2024-05-15 12:47:28 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = try(aws_vpc.main[0].id, null)
-}
-
-output "public_subnet" {
-  description = "The ID of the VPC"
-  value       = try(aws_subnet.public_subnets[0].id, null)
+  value       = aws_vpc.main.id
 }
